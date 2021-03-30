5243e2f1e59c184b9d1d94e632182fd59497b7c6
김호정 <hjnk125@gmail.com>
2021-03-30 16:56:56 +0900

    [Quiz App]Add score feature 5문제의 퀴즈 답변 제출 후 점수 퍼센트를 보여줌

--- src/Components/Quiz.tsx
+++ src/Components/Quiz.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import './Quiz.scss';
 
@@ -50,6 +50,11 @@ const Quiz = ({ history }: RouteComponentProps) => {
   const [questionNum, setQuestionNum] = useState<number>(0)
   const [fullPage, setFullPage] = useState<boolean>(false);
 
+  const [userAnswers, setUserAnswers] = useState<Array<string>>([]);
+  const [newAnswer, setNewAnswer] = useState<string>('');
+
+  let score: number = 0;
+
   return (
     <div className="background">
       <div className={(fullPage) ? "quiz-container-full" : "quiz-container"}>
@@ -57,56 +62,86 @@ const Quiz = ({ history }: RouteComponentProps) => {
         <div className="titlebar">
           <div className="buttons">
             <div className="close">
-              <a className="closebutton" href="#" onClick={() => {
-                history.push('/');
-              }}><span>x</span></a>
+              <a className="closebutton" href="#!" onClick={() => { history.push('/'); }}><span>x</span></a>
             </div>
             <div className="minimize">
-              <a className="minimizebutton" href="#" onClick={() =>
-                setFullPage(false)
-              }><span>&ndash;</span></a>
+              <a className="minimizebutton" href="#!" onClick={() => setFullPage(false)}><span>&ndash;</span></a>
             </div>
             <div className="zoom">
-              <a className="zoombutton" href="#" onClick={() =>
-                setFullPage(true)
-              }><span>+</span></a>
+              <a className="zoombutton" href="#!" onClick={() => setFullPage(true)}><span>+</span></a>
             </div>
           </div>
           <span className="titlebar-title">Quiz about me!</span>
         </div>
 
+        {(questionNum < 5) ?
+          (<div>
+            {/* 문제영역 */}
+            <h3>{questionBox[questionNum].question}</h3>
+            <div className="question-box">
+              <div>
+                <input type="radio" id="a" name="answer" value="a" onChange={(e) => setNewAnswer(e.target.value)} />
+                <label htmlFor="a">{questionBox[questionNum].a}</label>
+              </div>
+              <div>
+                <input type="radio" id="b" name="answer" value="b" onChange={(e) => setNewAnswer(e.target.value)} />
+                <label htmlFor="b">{questionBox[questionNum].b}</label>
+              </div>
+              <div>
+                <input type="radio" id="c" name="answer" value="c" onChange={(e) => setNewAnswer(e.target.value)} />
+                <label htmlFor="c">{questionBox[questionNum].c}</label>
+              </div>
+              <div>
+                <input type="radio" id="d" name="answer" value="d" onChange={(e) => setNewAnswer(e.target.value)} />
+                <label htmlFor="d">{questionBox[questionNum].d}</label>
+              </div>
+            </div>
+
+            <h4>{questionNum + 1} / 5</h4>
+            <button onClick={() => {
+              let temp: Array<string> = userAnswers.slice();
+              temp.push(newAnswer);
+              setUserAnswers(temp);
+
+              setQuestionNum(questionNum + 1);
+
+              if (questionNum === 4) {
+                alert('last question');
+              }
+            }}>next</button>
+          </div>) :
+          (<div>
+            {/* 채점결과 */}
+            <br />
+            {userAnswers.map((ele, i) => {
+              let color = 'green';
+              if (questionBox[i].correct === ele) {
+                score += 20;
+                color = 'green';
+              } else {
+                color = 'FireBrick'
+              }
+
+              return (<div key={i} className={(color === 'green') ? "answer correct" : "answer wrong"} >
+                <strong>{i + 1}. {questionBox[i].question}</strong>
+                <div>
+                  correct: {questionBox[i].correct}
+                </div>
+                <div>
+                  your answer: <span style={{ color: color, fontWeight: 600 }}>{ele}</span>
+                </div>
+              </div>)
+            })}
+
+            <h4>🎉 score: {score} / 100 🎉</h4>
+            <br />
+          </div>)
+        }
 
 
-        <h3>{questionBox[questionNum].question}</h3>
-        <div className="question-box">
-          <div>
-            <input type="radio" id="a" name="answer" value="a" />
-            <label htmlFor="a">{questionBox[questionNum].a}</label>
-          </div>
-          <div>
-            <input type="radio" id="b" name="answer" value="b" />
-            <label htmlFor="b">{questionBox[questionNum].b}</label>
-          </div>
-          <div>
-            <input type="radio" id="c" name="answer" value="c" />
-            <label htmlFor="c">{questionBox[questionNum].c}</label>
-          </div>
-          <div>
-            <input type="radio" id="d" name="answer" value="d" />
-            <label htmlFor="d">{questionBox[questionNum].d}</label>
-          </div>
-        </div>
 
-        <div>{questionNum + 1} / 5</div>
-        <button onClick={() => {
-          if (questionNum < 4) {
-            setQuestionNum(questionNum + 1);
-          } else {
-            alert('last question');
-          }
-        }}>next</button>
       </div>
-    </div>
+    </div >
   )
 };
 
